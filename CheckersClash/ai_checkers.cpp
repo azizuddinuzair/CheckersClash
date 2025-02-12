e4845da3fb567d1451620fdd2b19665ce5ad8fc1
Uzair Azizuddin <azizuddinuzair@gmail.com>
2025-02-12 15:01:37 -0600

    CheckersGame()

--- CheckersClash/ai_checkers.cpp
+++ CheckersClash/ai_checkers.cpp
@@ -2,3 +2,30 @@
 #include <iostream>
 #include <algorithm>
 #include <climits>
+
+
+CheckersGame::CheckersGame() : blackTurn(false) {
+    // Initialize empty board
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            board[i][j] = PieceType::EMPTY;
+        }
+    }
+    
+    // Place initial pieces
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            if ((row + col) % 2 == 1) {
+                board[row][col] = PieceType::RED;
+            }
+        }
+    }
+    
+    for (int row = 5; row < BOARD_SIZE; row++) {
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            if ((row + col) % 2 == 1) {
+                board[row][col] = PieceType::BLACK;
+            }
+        }
+    }
+}
